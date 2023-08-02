604b1e4682c7ead8f3bc020be1c683df376d7404
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-08-02 22:42:53 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+// vite.config.js
+import { defineConfig } from "vite";
+// import htmlPlugin from "vite-plugin-html";
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+import postcssNesting from "postcss-nesting";
+import path from "path";
+
+export default defineConfig({
+  base: "./",
+  plugins: [
+    // htmlPlugin({
+    //   // Add your configuration options here (see options below).
+    // }),
+    handlebars({
+      partialDirectory: resolve(__dirname, "src", "partials"),
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [postcssNesting],
+    },
+  },
+});
