6b538c2f8884f3efdfcd892eb21ddcf959f94c0f
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-08-10 22:23:33 +0300

    main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
+import { initRegistration } from './registration';
 import { initHeroSlider } from './hero-slider';
 import { initMobileMenu } from './mobile-menu';
+import { initModal } from './modal';
+import { smoothScroll } from './smooth-scroll';
 
 initMobileMenu();
 initHeroSlider();
+smoothScroll();
+initModal();
+initRegistration();
